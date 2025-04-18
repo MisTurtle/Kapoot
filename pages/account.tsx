ca9c599663f7aa60c35a5c2671789628e0342d3d
Siphy666 <Siphy666@gmail.com>
2025-04-18 19:09:19 +0200

    first style for account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -4,6 +4,7 @@ import { useAuth } from "@contexts/AuthContext";
 import { useRouter } from "next/router";
 import { handle } from "@common/responses";
 import { useEffect, useState } from 'react';
+import Link from "next/link";
 
 import styles from './account.module.scss';
 import HeroPage from "@components/wrappers/HeroPage";
@@ -20,9 +21,18 @@ const AccountContent = () => {
   const [quizzes, setQuizzes] = useState<SimpleQuizzComponent[]>([]);
   const [quizzIds, setQuizzIds] = useState<string[]>([])
   const [quizzTmsp, setQuizzTmsp] = useState<string[][]>([]);
+  const [shownQuestions, setShownQuestions] = useState<boolean[]>([]);
   if (loading) return <p>Chargement...</p>;
 
   const editQuizz = (quizzId: string) => `/editor?quizz=${quizzId}`;
+
+  const toggleQuestions = (index: number) => {
+    setShownQuestions((prev) => {
+      const updated = [...prev];
+      updated[index] = !updated[index];
+      return updated;
+    });
+  };
 
   // Gets user quizzes
   useEffect(() => {
@@ -36,6 +46,7 @@ const AccountContent = () => {
         const quizzTmsp = result?.map(quizz => [quizz.created_at, quizz.updated_at]) ?? [];
 
         setQuizzIds(quizzIds);
+        setShownQuestions(new Array(deserializedQuizzes.length).fill(false));
         setQuizzes(deserializedQuizzes);
         setQuizzTmsp(quizzTmsp);
       },
@@ -114,64 +125,103 @@ const AccountContent = () => {
     return `${month} ${day}${suffix}, ${year} at ${hours}:${minutes}`;
   };
 
+  const createQuizz = () => {
+          if(!user) {
+              router.push("/login");
+              return;
+          }
+  
+          fetch("/api/editor/quizz", { method: "POST" }).then(async (res) =>
+              await handle<{ identifier: string }>(
+                  res,
+                  (result?) => {
+                      if (!result) throw new Error("Result should always be defined for route POST /api/editor/quizz");
+                      showPopup("success", "New quizz created!", 5.0);
+                      router.push(`/editor?quizz=${result.identifier}`);
+                  },
+                  (err) => {
+                      showPopup("error", err, 5.0);
+                  }
+              )
+          );
+      };
+
   if(!user) return <Loading />;
   return (
       <>
-        <header className={styles.titleSection}>
-          <h1 className={styles.titlePage}>Your account</h1>
-          <CustomNavBar links={['home']} />
-        </header>
-
-        <HeroPage className={styles.heroContent}>
-         
+        
+        <div className={styles.accountPage}> 
+          <header className={styles.titleSection}>
+            <h1 className={styles.titlePage}>Your account</h1>
+            <CustomNavBar links={['home']} />
+          </header>        
           <div className={styles.accountTop}>
-            <div className={styles.accountAvatar}>
-              <img src={ '/public/images/users' + userData?.avatar} alt="Avatar"></img>
+            
+            <div className={styles.accountUserInfo}>
+              <div className={styles.accountAvatar}>
+                <img src={ '/public/images/users' + userData?.avatar} alt="Avatar"></img>
+              </div>
+              <div className={styles.accountData}>
+                <span className={styles.accountUsername}>{user.username}</span>
+                <span className={styles.accountEmail}>{user.mail}</span>
+              </div>
             </div>
-            <div className={styles.accountData}>
-              <span className={styles.accountUsername}>{user.username}</span>
-              <span className={styles.accountEmail}>Email: {user.mail}</span>
-              <span className={styles.accountNbQuizzes}>QuizzCreated: {userData?.quizzes_created}</span>
-              <span className={styles.accountNbGames}>games_played: {userData?.games_played}</span>
-              <span className={styles.accountNbPoints}>total_points: {userData?.total_points}</span>
+            <div className={styles.accountStatsList}>
+              <span className={styles.accountStatsItem}>{userData?.quizzes_created} quizz{userData?.quizzes_created > 1 ? 'es' : ''} created</span>
+              <span className={styles.accountStatsItem}>|</span>
+              <span className={styles.accountStatsItem}>{userData?.games_played} game{userData?.games_played > 1 ? 's' : ''} played</span>
+              <span className={styles.accountStatsItem}>|</span>
+              <span className={styles.accountStatsItem}>{userData?.total_points} point{userData?.total_points > 1 ? 's' : ''} earned</span> 
             </div>
           </div>
-          
-          <div className={styles.quizzSection}>
-            
             {quizzes.length > 0 ? (
-                <div className={styles.userQuizzContainer}>
-                  <span className={styles.quizzContainerTitle}>Quizzes :</span>
-                    {quizzes.map((quizz, index) => (                      
-                      <div className={styles.quizzContainer} key={index}>
-                        <div className={styles.quizzData}>
-                          <a className={styles.editQuizz} href={editQuizz(quizzIds[index])}>{quizz.get("label")}</a>
-                          <p>{quizz.get("description")}</p>
-                          <button className={styles.removeQuizz} onClick={() => removeQuizz(quizzIds[index])}>DELETE</button>
-                        </div>
-                        <div className={styles.quizzTime}>
-                          <span className={styles.dateQuizz}>Created on { formatDateWithOrdinal(quizzTmsp[index][0])}</span>
-                          <span className={styles.updatedQuizz}> Last update on {formatDateWithOrdinal(quizzTmsp[index][1])}</span>
-                        </div>
-                        <div className={styles.questionsContainer}>
-                        {quizz.children && quizz.children.length > 0 ? (
-                          quizz.children.map((question, idx) => (
-                            <div className={styles.question} key={idx}>
-                              <p>{question.get("label")}</p>
-                            </div>
+              <div className={styles.userQuizzContainer}>
+                <span className={styles.quizzContainerTitle}>Your quizzes</span>
+                {quizzes.map((quizz, index) => (                      
+                  <div className={styles.quizzContainer} key={index}>
+                    <div className={styles.quizzSection}>
+                      <div className={styles.quizzLeft}>
+                        <a className={styles.editQuizz} href={editQuizz(quizzIds[index])}>{quizz.get("label")}</a>
+                        {/* <p>{quizz.get("description")}</p> */}
+                      </div>
+                      <div className={styles.quizzTime}>
+                        <span className={styles.dateQuizz}>Created on { formatDateWithOrdinal(quizzTmsp[index][0])}</span>
+                        <span className={styles.dateQuizz}> Last update on {formatDateWithOrdinal(quizzTmsp[index][1])}</span>
+                      </div>
+                      <div className={styles.quizzQuestions}>
+                        <span className={styles.quizzQuestionLabel}>{ quizz.children.length} question{quizz.children.length > 1 ? 's' : ''}</span>
+                        <span className={styles.quizzShowQuestion} onClick={() => toggleQuestions(index)}>Show/hide question{quizz.children.length > 1 ? 's' : ''}</span>
+                      </div>
+                      <div className={styles.quizzRigth}>
+                        <button className={styles.removeQuizz} onClick={() => removeQuizz(quizzIds[index])}>DELETE</button>
+                      </div>
+                    </div>
+                    {shownQuestions[index] && (
+                    <div className={styles.questionSection}>
+                      {/* <div className={styles.questionsContainer}> */}
+                        {quizz.children.length > 0 ? ( quizz.children.map((question, idx) => (
+                          <div className={styles.question} key={idx}>
+                            <span>Question {idx+1} : {question.get("label")}</span>
+                          </div>
+                          
                           ))
                         ) : (
-                            <p>This quizz does not contain any question</p>
-                          )}
-                        </div>
-                      </div>
-                    ))}
-                </div>
+                          <p>This quizz does not contain any question</p>
+                        )}
+                      {/* </div> */}
+                    </div>
+                    )}
+                  </div>
+                  
+                ))}
+              </div>
             ) : (
-                <p>No quizz</p>
+              <div className={styles.noQuizz}>
+                <span className={styles.noQuizzLabel}>You haven't created a quizz yet</span>
+                <p className={styles.quizzCreateParagraph}>Want to create your own quizz? It's over <span onClick={createQuizz}> here!</span></p>
+              </div>
             )}
-          </div>
-        </HeroPage>
+        </div> 
       </>
   );
 };
