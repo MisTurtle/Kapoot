17625c6847e51605d178ed945600cc19da5ad5db
Siphy666 <Siphy666@gmail.com>
2025-04-20 15:07:53 +0200

    styles for page to create username

--- pages/create-username.tsx
+++ pages/create-username.tsx
@@ -1,14 +1,18 @@
-
 import { useRouter } from "next/router";
 import { useState, useEffect } from "react";
 import { handle } from "@common/responses";
 import { usePopup } from "@contexts/PopupContext";
+import { randomIGN } from "@server/utils/ign";
+import styles from "./create-username.module.scss";
+import HeroPage from "@components/wrappers/HeroPage";
 
 const EnterUsername = () => {
     const router = useRouter();
     const { showPopup } = usePopup();
     const [code, setCode] = useState<string | undefined>(undefined);
-    const [username, setUsername] = useState("");
+    const [username, setUsername] = useState<string>("");
+
+    const random_username = randomIGN();
 
     useEffect(() => {
         if (router.query.gamecode) {
@@ -18,40 +22,43 @@ const EnterUsername = () => {
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
-        
-        if (!username.trim() || !code) return;
-    
+
+        const user_name = username.trim() === "" ? random_username : username;
+
+        if (!code) return;
+
         const res = await fetch(`/api/game/${code}`, {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ username }),
+            body: JSON.stringify({ username: user_name }),  
         }).then(async res => handle(
             res,
             (result) => {
+                showPopup('success', 'Successfully joined the game !', 5.0);
                 router.push('/game');
             },
             (error) => {
                 showPopup('error', error, 5.0);
             }
-        ));;
+        ));
     };
-  
 
     return (
-    <form onSubmit={handleSubmit} style={{ padding: "2rem" }}>
-      <h2>Enter your pseudonym before enter the game</h2>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Your pseudonym"
-        required
-      />
-      <button type="submit">Rejoindre</button>
-    </form>
-  );
+        <HeroPage className={styles.heroContent}>
+        <form className={styles.codeInput} onSubmit={handleSubmit}>
+            <span className={styles.title}>ENTER YOUR PSEUDONYM</span>
+            <input className={styles.enterCode}
+                type="text"
+                value={username}  
+                onChange={(e) => setUsername(e.target.value)} 
+                placeholder={random_username}  
+            />
+            <button className={styles.startButton} type="submit">Join the game</button>
+        </form>
+        </HeroPage>
+    );
 };
 
 export default EnterUsername;
